dc3dc03db03a2c5bf66aeb238328cdbdd5432c90
Akshat Samdani <akshatsamdani23@gmail.com>
2024-09-05 09:36:52 +0000

    added angular material and added edit delete

--- todo-ui/src/app/app.component.ts
+++ todo-ui/src/app/app.component.ts
@@ -10,16 +10,41 @@ import { RouterOutlet } from '@angular/router';
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss' 
 }) 
-export class AppComponent { 
-    title = 'TODO App'; 
-    taskName: string = ''; 
-    tasks: string[] = [];
+export class AppComponent {
+  title = 'Todo App';
+  taskName: string = '';
+  taskDescription: string = '';
+  tasks: { name: string, description: string }[] = [];
+  isEditMode: boolean = false;
+  editIndex: number | null = null;
 
-    addTask(event: Event): void { 
-        event.preventDefault(); 
-        if (this.taskName.trim()) { 
-            this.tasks.push(this.taskName); 
-            this.taskName = '';
-        }
+  addOrUpdateTask(event: Event): void {
+    event.preventDefault();
+    if (this.isEditMode && this.editIndex !== null) {
+      this.tasks[this.editIndex] = { name: this.taskName, description: this.taskDescription };
+      this.isEditMode = false;
+      this.editIndex = null;
+    } else {
+      this.tasks.push({ name: this.taskName, description: this.taskDescription });
     }
-} 
+    this.taskName = '';
+    this.taskDescription = '';
+  }
+
+  editTask(index: number): void {
+    this.taskName = this.tasks[index].name;
+    this.taskDescription = this.tasks[index].description;
+    this.isEditMode = true;
+    this.editIndex = index;
+  }
+
+  deleteTask(index: number): void {
+    this.tasks.splice(index, 1);
+    if (this.isEditMode && this.editIndex === index) {
+      this.isEditMode = false;
+      this.editIndex = null;
+      this.taskName = '';
+      this.taskDescription = '';
+    }
+  }
+}
