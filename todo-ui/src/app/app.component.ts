f4d975f9de56c7a46fec32f847ab026a9da7b63c
Akshat Samdani <akshatsamdani23@gmail.com>
2024-09-05 09:28:30 +0000

    Initial ui setup

--- todo-ui/src/app/app.component.ts
+++ todo-ui/src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
+import { Component } from '@angular/core'; 
+import { FormsModule } from '@angular/forms'; 
 import { RouterOutlet } from '@angular/router';
 
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
-})
-export class AppComponent {
-  title = 'todo-ui';
-}
+@Component({ 
+    selector: 'app-root', 
+    standalone: true, 
+    imports: [RouterOutlet, FormsModule, CommonModule], 
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss' 
+}) 
+export class AppComponent { 
+    title = 'TODO App'; 
+    taskName: string = ''; 
+    tasks: string[] = [];
+
+    addTask(event: Event): void { 
+        event.preventDefault(); 
+        if (this.taskName.trim()) { 
+            this.tasks.push(this.taskName); 
+            this.taskName = '';
+        }
+    }
+} 
