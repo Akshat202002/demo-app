7eb63fd2d6f4bf88a7c813755a5b57ca7bca0bd0
Akshat Samdani <akshatsamdani23@gmail.com>
2024-09-08 14:16:47 +0000

    updated style cards

--- todo-ui/src/app/app.component.ts
+++ todo-ui/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { CommonModule } from '@angular/common'; 
+ 
 import { Component, OnInit } from '@angular/core'; 
 import { FormsModule } from '@angular/forms'; 
 import { RouterOutlet } from '@angular/router';
@@ -7,7 +7,7 @@ import { TodoService, Todo } from './services/todo.service';
 @Component({ 
     selector: 'app-root', 
     standalone: true, 
-    imports: [RouterOutlet, FormsModule, CommonModule], 
+    imports: [RouterOutlet, FormsModule], 
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'] 
 }) 
