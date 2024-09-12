029d5bfc2ba499854372a123b062bd0b9d835044
akshat-samdani <136007962+akshat-samdani@users.noreply.github.com>
2024-09-12 21:07:00 +0530

    using hibernate

--- todo-backend/src/main/java/com/akshat/todo_backend/repository/TodoRepository.java
+++ todo-backend/src/main/java/com/akshat/todo_backend/repository/TodoRepository.java
@@ -1,11 +1,81 @@
 package com.akshat.todo_backend.repository;
-import com.akshat.todo_backend.model.Todo;
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-public interface TodoRepository extends JpaRepository<Todo, Long> {
-    @Query("SELECT t FROM Todo t WHERE t.name LIKE %:keyword% OR t.description LIKE %:keyword%")
-    List<Todo> searchTodos(@Param("keyword") String keyword);
+import com.akshat.todo_backend.model.Todo;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import jakarta.persistence.EntityManagerFactory;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class TodoRepository {
+
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public TodoRepository(EntityManagerFactory factory) {
+        this.sessionFactory = factory.unwrap(SessionFactory.class);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Todo> findAll() {
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery("from Todo", Todo.class).list();
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<Todo> findById(Long id) {
+        try (Session session = sessionFactory.openSession()) {
+            return Optional.ofNullable(session.get(Todo.class, id));
+        }
+    }
+
+    @Transactional
+    public Todo save(Todo todo) {
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            session.persist(todo);
+            session.getTransaction().commit();
+            return todo;
+        }
+    }
+
+    @Transactional
+    public void deleteById(Long id) {
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            Todo todo = session.get(Todo.class, id);
+            if (todo != null) {
+                session.remove(todo);
+            }
+            session.getTransaction().commit();
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public List<Todo> searchTodos(String keyword) {
+        try (Session session = sessionFactory.openSession()) {
+            return session
+                    .createQuery("from Todo t where t.name like :keyword or t.description like :keyword", Todo.class)
+                    .setParameter("keyword", "%" + keyword + "%")
+                    .list();
+        }
+    }
+
+    // Using Criteria API
+    // @Transactional(readOnly = true)
+    // public List<Todo> searchTodos(String keyword) {
+    // try (Session session = sessionFactory.openSession()) {
+    // return session.createQuery(
+    // "from Todo t where t.name like :keyword or t.description like :keyword",
+    // Todo.class)
+    // .setParameter("keyword", "%" + keyword + "%")
+    // .list();
+    // }
+    // }
 }
